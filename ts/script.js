b1f4de5538c9a150541f88003c42c6b52bbfd103
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2023-12-18 16:29:07 -0800

    ts initiated

---
+++ ts/script.js
@@ -0,0 +1,6 @@
+// main.ts
+function greet(name) {
+    return "Hello, ".concat(name, "!");
+}
+var message = greet("World");
+console.log(message);
---
+++ ts/script.ts
@@ -0,0 +1,7 @@
+// main.ts
+function greet(name: string): string {
+    return `Hello, ${name}!`;
+}
+
+const message = greet("World");
+console.log(message);
