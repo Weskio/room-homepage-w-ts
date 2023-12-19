b98c292fae5c3577ddc746ce52daf4bf00660e64
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2023-12-19 15:08:05 -0800

    functionality

--- ts/script.ts
+++ ts/script.ts
@@ -11,7 +11,12 @@ class MySlider {
             title: 'We are available all across the globe',
             description: 'With stores all over the world, it\'s easy for you to find furniture for your home...'
         },
-        // ... more slides ...
+        {
+            mobileBackground: 'mobile3',
+            title: 'Manufactured with the best materials',
+            description: 'Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.'
+        },
+        
     ];
 
     constructor() {
