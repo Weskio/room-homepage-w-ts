4a66ea0ba0d89fcb287cc555633e85a819a284da
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2023-12-19 14:39:32 -0800

    ...

--- ts/script.js
+++ ts/script.js
@@ -1,22 +1,4 @@
 "use strict";
-class MenuToggle {
-    constructor() {
-        this.initEventListeners();
-    }
-    initEventListeners() {
-        const toggleButtons = document.querySelectorAll('.toggle');
-        toggleButtons.forEach(button => {
-            button.addEventListener('click', () => this.toggleMenu());
-        });
-    }
-    toggleMenu() {
-        const menu = document.querySelector('.desktop-header');
-        const shadow = document.querySelector('.shadow');
-        menu === null || menu === void 0 ? void 0 : menu.classList.toggle('active');
-        shadow === null || shadow === void 0 ? void 0 : shadow.classList.toggle('active');
-    }
-}
-const menuToggle = new MenuToggle();
 class MySlider {
     constructor() {
         this.currentIndex = 0;
@@ -24,20 +6,17 @@ class MySlider {
             {
                 mobileBackground: 'mobile1',
                 title: 'Discover innovative ways to decorate',
-                description: "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love"
+                description: 'We provide unmatched quality, comfort, and style for property owners across the country...'
             },
             {
                 mobileBackground: 'mobile2',
                 title: 'We are available all across the globe',
-                description: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today."
+                description: 'With stores all over the world, it\'s easy for you to find furniture for your home...'
             },
-            {
-                mobileBackground: 'mobile3',
-                title: 'Manufactured with the best materials',
-                description: "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office."
-            },
+            // ... more slides ...
         ];
         this.initEventListeners();
+        this.updateSlide(); // Initialize the first slide
     }
     initEventListeners() {
         const leftButton = document.querySelector('.button .btn:first-child');
@@ -52,7 +31,7 @@ class MySlider {
         if (titleElement && descriptionElement && backgroundElement) {
             titleElement.textContent = this.slides[this.currentIndex].title;
             descriptionElement.textContent = this.slides[this.currentIndex].description;
-            backgroundElement.className = `${this.slides[this.currentIndex].mobileBackground}`;
+            backgroundElement.className = `hero ${this.slides[this.currentIndex].mobileBackground}`;
         }
     }
     nextSlide() {
--- ts/script.ts
+++ ts/script.ts
@@ -1,50 +1,22 @@
-class MenuToggle {
-    constructor() {
-        this.initEventListeners();
-    }
-
-    private initEventListeners(): void {
-        const toggleButtons = document.querySelectorAll('.toggle');
-        toggleButtons.forEach(button => {
-            button.addEventListener('click', () => this.toggleMenu());
-        });
-    }
-
-    private toggleMenu(): void {
-        const menu = document.querySelector('.desktop-header');
-        const shadow = document.querySelector('.shadow');
-
-        menu?.classList.toggle('active');
-        shadow?.classList.toggle('active');
-    }
-}
-
-const menuToggle = new MenuToggle();
-
-
 class MySlider {
     private currentIndex: number = 0;
     private slides: { mobileBackground: string; title: string; description: string; }[] = [
         {
-        mobileBackground:'mobile1', 
-        title:'Discover innovative ways to decorate',
-        description:"We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love"
-    },
-    {
-        mobileBackground:'mobile2', 
-        title:'We are available all across the globe',
-        description:"With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today."
-    },
-    {
-        mobileBackground:'mobile3', 
-        title:'Manufactured with the best materials',
-        description:"Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office."
-    },
-
+            mobileBackground: 'mobile1',
+            title: 'Discover innovative ways to decorate',
+            description: 'We provide unmatched quality, comfort, and style for property owners across the country...'
+        },
+        {
+            mobileBackground: 'mobile2',
+            title: 'We are available all across the globe',
+            description: 'With stores all over the world, it\'s easy for you to find furniture for your home...'
+        },
+        // ... more slides ...
     ];
 
     constructor() {
         this.initEventListeners();
+        this.updateSlide(); // Initialize the first slide
     }
 
     private initEventListeners(): void {
@@ -63,7 +35,7 @@ class MySlider {
         if (titleElement && descriptionElement && backgroundElement) {
             titleElement.textContent = this.slides[this.currentIndex].title;
             descriptionElement.textContent = this.slides[this.currentIndex].description;
-            backgroundElement.className = `${this.slides[this.currentIndex].mobileBackground}`;
+            backgroundElement.className = `hero ${this.slides[this.currentIndex].mobileBackground}`;
         }
     }
 
