cf0a90561403c69b176f697ff2bf24469bd8243e
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2023-12-20 08:54:58 -0800

    content addition

--- ts/script.ts
+++ ts/script.ts
@@ -39,7 +39,7 @@ class MySlider {
         {
             mobileBackground: 'mobile1',
             title: 'Discover innovative ways to decorate',
-            description: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today."
+            description: "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love"
         },
         {
             mobileBackground: 'mobile2',
